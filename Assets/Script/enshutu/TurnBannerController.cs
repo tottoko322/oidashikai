f1b10f3797a10af253138e4683e74ff01d7f6b6a
tottoko322 <mercury8ly@icloud.com>
2026-03-08 22:06:03 +0900

    torimakannsei

--- Assets/Script/enshutu/TurnBannerController.cs
+++ Assets/Script/enshutu/TurnBannerController.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using TMPro;
 
@@ -6,19 +5,15 @@ public class TurnBannerController : MonoBehaviour
 {
     public GameObject root;
     public TMP_Text text;
-    public float showTime = 0.8f;
 
-    public void Show(string message)
-    {
-        StopAllCoroutines();
-        StartCoroutine(CoShow(message));
-    }
-
-    private IEnumerator CoShow(string msg)
+    private void Awake()
     {
         if (root) root.SetActive(true);
-        if (text) text.text = msg;
-        yield return new WaitForSeconds(showTime);
-        if (root) root.SetActive(false);
+    }
+
+    public void SetText(string message)
+    {
+        if (root) root.SetActive(true);
+        if (text) text.text = message;
     }
 }
