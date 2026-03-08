dce74f1b31116697f22742e9a0c6cac8a19f5515
Ximr <tkutk1392@gmail.com>
2026-03-08 19:22:48 +0900

    [ADD]add SE
    SEの追加
    AudioManagerの追加

---
+++ Assets/Script/Title/btn/btnClick.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BtnClick : MonoBehaviour
+{
+    private AudioManager audioManager;
+
+    public void Start()
+    {
+        audioManager=AudioManager.I;
+    }
+
+    public void Click()
+    {
+        if (audioManager != null)
+        {
+            audioManager.PlayClick();
+        }
+    }
+        public void Cnacel()
+    {
+        if (audioManager != null)
+        {
+            audioManager.PlayClick();
+        }
+    }
+}
