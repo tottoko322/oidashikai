66652823384c627ae4b653c08435f7bef9df5a8a
Ximr <tkutk1392@gmail.com>
2026-03-12 06:46:41 +0900

    [Add]add sorting system on dic
    図鑑にソート機能を追加
    細かな修正

--- Assets/Dic/DicManager.cs
+++ Assets/Dic/DicManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -10,20 +11,24 @@ public class DicManager : MonoBehaviour
     public GameObject card;
     public Transform content;
     public GameObject cardViewFB;
+    public List<Transform> cardList;
     public static GameObject cardView;
     public static bool allLoaded=false;
+    public GameObject sortOptionUI;
     AsyncOperationHandle<IList<CardData>> handle;
     private void Awake()
     {
         cardView=cardViewFB;
         if(!cardView)return;
         cardView.SetActive(false);
+        if(!sortOptionUI)return;
+        sortOptionUI.SetActive(false);
     }
     void Start()
     {
         handle=Addressables.LoadAssetsAsync<CardData>(label, addressable =>
         {
-            Debug.Log("loaded"+addressable.displayName);
+            Debug.Log("loaded : "+addressable.displayName);
             addCard(addressable);
         });
         handle.Completed += (AsyncOperationHandle<IList<CardData>> obj) =>
@@ -33,6 +38,7 @@ public class DicManager : MonoBehaviour
             {
                 Debug.Log("All Data Loaded");
                 allLoaded=true;
+                Sorting("alphabet"); //default value
             }
         };
     }
@@ -44,6 +50,7 @@ public class DicManager : MonoBehaviour
             if(obj.GetComponent<CardView>() is CardView view)
             {
                 view.Bind(data);
+                obj.name=data.displayName;
             }
         }
     }
@@ -53,6 +60,10 @@ public class DicManager : MonoBehaviour
         Addressables.Release(handle);
         Debug.Log("All Data Released");
     }
+    public void ToggleSortingUI()
+    {
+        sortOptionUI.SetActive(!sortOptionUI.activeSelf);
+    }
 
     public static void ShowCards(Image src)
     {
@@ -61,5 +72,51 @@ public class DicManager : MonoBehaviour
         img.sprite=src.sprite;
     }
 
-
+    public List<Transform> GetContentChildren()
+    {
+        if(!(cardList.Count<=0))return cardList;
+        List<Transform> list = new List<Transform>();
+        for(int i = 0; i < content.childCount; i++)
+        {
+            list.Add(content.GetChild(i));
+        }
+        return list;
+    }
+        public void SetContentChildren(List<Transform> sortedList)
+    {
+        foreach(Transform elem in sortedList)
+        {
+            elem.transform.SetAsLastSibling();
+        }
+    }
+    public List<Transform> SortBy(List<Transform> list,string orderRule)
+    {
+        if(list.Count<=0)return list;
+        switch (orderRule){
+        case "alphabet":
+            list.Sort((e1,e2)=>string.Compare(e1.name,e2.name));
+            return list;
+        case "atk":
+            list.Sort((e1,e2)=>int.Parse(getAtk(e1)).CompareTo(int.Parse(getAtk(e2))));
+            return list;
+        case "def":
+            list.Sort((e1,e2)=>int.Parse(getDef(e1)).CompareTo(int.Parse(getDef(e2))));
+            return list;
+        default:
+            return list;
+        }
+    }
+    private string getAtk(Transform trans)
+    {
+        return trans.GetComponent<CardView>().atkText.text;
+    }
+    private string getDef(Transform trans)
+    {
+        return trans.GetComponent<CardView>().defText.text;
+    }
+    public void Sorting(string orderRule)
+    {
+        cardList=GetContentChildren();
+        SetContentChildren(SortBy(cardList,orderRule));
+    }
 }
